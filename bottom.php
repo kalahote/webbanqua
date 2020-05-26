6fd0418a1c6e80d49b64ec58f5f8e20cda0e27e3
kalahote <44584475+kalahote@users.noreply.github.com>
2020-05-26 23:23:35 +0700

    Delete bottom.php

--- bottom.php
+++
@@ -1,9 +0,0 @@
-<table width="1200" height="auto" cellpadding="0" cellspacing="0" border="0" style="border:#CCCCCC 1px solid; margin-top:10px;">
-	<tr>
-		<td height="97" valign="middle" align="center" style="color:white;text-align:center">
-		Copyright © Nguyen Quoc Trung. <br/>
-		Công Nghệ Phần Mềm.		
-				
-		</td>		
-	</tr>
-</table>
